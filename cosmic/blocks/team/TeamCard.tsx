2d823b0b19ee30411f53f53cf17d694354568a53
Hugo Vitor <hugo.nunes@geap.com.br>
2025-02-28 11:45:48 -0300

    Alteração de logo instagram

--- cosmic/blocks/team/TeamCard.tsx
+++ cosmic/blocks/team/TeamCard.tsx
@@ -11,8 +11,7 @@ export type MemberType = {
     position: string;
     bio: string;
     links: {
-      x: string;
-      linkedin: string;
+      instagram: string;
     };
   };
 };
@@ -48,44 +47,20 @@ export function TeamCard({
           {member.metadata.bio}
         </p>
         <div className="flex justify-start space-x-4 py-4">
-          {member.metadata.links.x && (
+          {member.metadata.links.instagram && (
             <a
-              href={member.metadata.links.x}
+              href={member.metadata.links.instagram}
               className="text-gray-500 hover:text-gray-600"
             >
               <svg
-                color="currentColor"
-                aria-label="X"
-                fill="currentColor"
-                height="22"
-                viewBox="0 0 22 20"
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0 0 50 50"
               >
-                <path
-                  d="M16.99 0H20.298L13.071 8.26L21.573 19.5H14.916L9.702 12.683L3.736 19.5H0.426L8.156 10.665L0 
-                    0H6.826L11.539 6.231L16.99 0ZM15.829 17.52H17.662L5.83 1.876H3.863L15.829 17.52Z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </a>
-          )}
-          {member.metadata.links.linkedin && (
-            <a
-              href={member.metadata.links.linkedin}
-              className="text-gray-500 hover:text-gray-600"
-            >
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-                <rect x="2" y="9" width="4" height="12"></rect>
-                <circle cx="4" cy="4" r="2"></circle>
+                <path d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z M 37 11 A 2 2 0 0 0 35 13 A 2 2 0 0 0 37 15 A 2 2 0 0 0 39 13 A 2 2 0 0 0 37 11 z M 25 14 C 18.936712 14 14 18.936712 14 25 C 14 31.063288 18.936712 36 25 36 C 31.063288 36 36 31.063288 36 25 C 36 18.936712 31.063288 14 25 14 z M 25 16 C 29.982407 16 34 20.017593 34 25 C 34 29.982407 29.982407 34 25 34 C 20.017593 34 16 29.982407 16 25 C 16 20.017593 20.017593 16 25 16 z"></path>
               </svg>
             </a>
           )}
